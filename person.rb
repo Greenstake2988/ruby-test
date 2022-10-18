7f3f55a414066279659bcbeaa85d6418d59960b5
Oscar Vazquez <0skr.vazquez@gmail.com>
2022-10-18 16:58:06 -0500

    modules

---
+++ person.rb
@@ -0,0 +1,32 @@
+class Person
+  PEOPLE = []
+  attr_reader :name, :hobbies, :friends
+  def initialize(name)
+    @name = name
+    @hobbies = []
+    @friends = []
+    PEOPLE << self
+  end
+  def has_hobby(hobby)
+    @hobbies << hobby
+  end
+  def hasfirend(friend)
+    @friend << friend
+  end
+  def Person.method_missing(m, *args)
+    method = m.to_s
+    if method.start_with?("all_with")
+      attr = method[9..-1]
+      if Person.public_method_defined?(attr)
+        PEOPLE.find_all do |person|
+          person.send(attr).include?(args[0])
+        end
+      else
+        raise ArgumentError, "No se encontro #{attr}"
+      end
+    else
+      super
+    end
+  end
+end
+
