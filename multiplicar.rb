cfd7c7ea6c1720d162d75fdf60ad2c4094586b97
Oscar Vazquez <0skr.vazquez@gmail.com>
2022-10-18 13:22:27 -0500

    Agregando cambios

---
+++ multiplicar.rb
@@ -0,0 +1,17 @@
+
+def multiplicar(a, b)
+  a * b
+end
+
+puts "Dame la primera variable"
+a = gets.chomp.to_i
+
+puts "Dame la segunda variable"
+b = gets.chomp.to_i
+
+print "Tu resultado es: "
+puts multiplicar(a,b)
+
+
+
+
---
+++ stack.rb
@@ -0,0 +1,6 @@
+require_relative "stacklike"
+
+class Stack
+  include Stacklike
+end
+
